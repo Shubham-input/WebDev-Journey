d688d2883ff238b0bd78e4782f1a9c66c7e7bbdd
Shubham Gupta <85833537+Shubham-input@users.noreply.github.com>
2026-05-13 14:22:37 +0000

    Completed the Simon Game

--- Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
+++ Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
@@ -9,10 +9,12 @@ $(".btn").on("click", function(){
     userClickedPattern.push(userChosenColour);
     animatePress(userChosenColour);
     playSound("sounds/" +userChosenColour + ".mp3");
+    checkAnswer(userClickedPattern.length-1);
     
 });
 
 function nextSequence(){
+    userClickedPattern = []
     level+=1;
     $("h1").text("Level " + level );
     let randomNumber = Math.floor(Math.random() * 4);
@@ -42,3 +44,31 @@ function animatePress(currentColour){
         nextSequence();
     }
 });
+function checkAnswer(currentLevel){
+    if (userClickedPattern[currentLevel] == gamePattern[currentLevel]){
+        console.log("success");
+
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function() {
+                nextSequence();
+            }, 1000);
+        }
+        
+    }
+    else{
+        console.log("Wrong");
+        let audio = new Audio('sounds/wrong.mp3');
+        audio.play();
+        $('body').addClass("game-over");
+        setTimeout(function(){
+            $('body').removeClass("game-over");
+        }, 200);
+        $("h1").text("Game Over, Press Any Key to Restart" );
+        startOver();
+    }
+}
+function startOver(){
+    level = 0;
+    gamePattern = [];
+    gameStarted = false;
+}
